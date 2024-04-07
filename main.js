5471e911cf812d03ed9d802eceaa9a206d13beee
Alaa2472 <alasamouly@gmail.com>
2024-04-08 01:04:50 +0200

    Oreo website first release

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById ("loader").style.display = "none";
+    document.getElementById("contentoz").style.display = "block";
+  }, 1500); // 3000 milliseconds (3 seconds)
+});
